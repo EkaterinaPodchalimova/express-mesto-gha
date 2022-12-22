7f9cea10b63fd3506f588703cadb30a3f58856aa
Ekaterina Podchalimova <pod4alimova.ekaterina@yandex.ru>
2022-12-22 19:20:24 +0700

    feat: исправлено card joi

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,38 @@
 const router = require('express').Router();
+const { celebrate, Joi, Segments } = require('celebrate');
+const auth = require('../middlewares/auth');
 const {
   getCard, deleteCardsById, createCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
-router.get('/', getCard);
-router.delete('/:cardId', deleteCardsById);
-router.post('/', createCard);
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', dislikeCard);
+router.get('/', celebrate({
+  [Segments.HEADERS]: Joi.object().keys({
+    authorization: Joi.string(),
+  }).unknown(true),
+}), auth, getCard);
+router.delete('/:cardId', celebrate({
+  [Segments.HEADERS]: Joi.object().keys({
+    authorization: Joi.string(),
+  }).unknown(true),
+}), auth, deleteCardsById);
+router.post('/', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().pattern(/(http|https):\/\/([\w.]+\/?)\S*/).required(),
+  }),
+  [Segments.HEADERS]: Joi.object().keys({
+    authorization: Joi.string(),
+  }).unknown(true),
+}), auth, createCard);
+router.put('/:cardId/likes', celebrate({
+  [Segments.HEADERS]: Joi.object().keys({
+    authorization: Joi.string(),
+  }).unknown(true),
+}), auth, likeCard);
+router.delete('/:cardId/likes', celebrate({
+  [Segments.HEADERS]: Joi.object().keys({
+    authorization: Joi.string(),
+  }).unknown(true),
+}), auth, dislikeCard);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -23,15 +23,15 @@ router.patch('/me', celebrate({
   [Segments.HEADERS]: Joi.object().keys({
     authorization: Joi.string(),
   }).unknown(true),
-}), editUsers);
+}), auth, editUsers);
 router.patch('/me/avatar', celebrate({
   [Segments.BODY]: Joi.object().keys({
-    avatar: Joi.string().uri().default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
+    avatar: Joi.string().pattern(/(http|https):\/\/([\w.]+\/?)\S*/).default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
   }),
   [Segments.HEADERS]: Joi.object().keys({
     authorization: Joi.string(),
   }).unknown(true),
-}), editAvatar);
+}), auth, editAvatar);
 router.get('/:userId', getUsersById);
 
 module.exports = router;
