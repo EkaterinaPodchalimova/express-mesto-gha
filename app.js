de9d57af821325d679ac6971b21fe475bc168164
Ekaterina Podchalimova <pod4alimova.ekaterina@yandex.ru>
2022-12-22 14:00:20 +0700

    feat: исправлен errors

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(errors);
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
@@ -20,6 +19,7 @@ app.post('/signup', postUsers);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
+app.use(errors());
 app.use((req, res, next) => {
   res.status(ERROR_CODE_404).send({ message: 'Страница не найдена' });
   next();
--- routes/users.js
+++ routes/users.js
@@ -1,40 +1,40 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
+const { celebrate, Joi, Segments } = require('celebrate');
 const {
   getUsers, getUsersById, getUserNow, editUsers, editAvatar,
 } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 
 router.get('/', celebrate({
-  headers: Joi.object().keys({
-    authorization: Joi.string().required(),
+  [Segments.HEADERS]: Joi.object().keys({
+    authorization: Joi.string(),
   }).unknown(true),
 }), auth, getUsers);
 router.get('/:userId', celebrate({
-  headers: Joi.object().keys({
-    authorization: Joi.string().required(),
+  [Segments.HEADERS]: Joi.object().keys({
+    authorization: Joi.string(),
   }).unknown(true),
 }), auth, getUsersById);
 router.get('/me', celebrate({
-  headers: Joi.object().keys({
-    authorization: Joi.string().required(),
+  [Segments.HEADERS]: Joi.object().keys({
+    authorization: Joi.string(),
   }).unknown(true),
 }), auth, getUserNow);
 router.patch('/me', celebrate({
-  body: Joi.object().keys({
+  [Segments.BODY]: Joi.object().keys({
     name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
     about: Joi.string().min(2).max(30).default('Исследователь'),
   }),
-  headers: Joi.object().keys({
-    authorization: Joi.string().required(),
+  [Segments.HEADERS]: Joi.object().keys({
+    authorization: Joi.string(),
   }).unknown(true),
 }), auth, editUsers);
 router.patch('/me/avatar', celebrate({
-  body: Joi.object().keys({
+  [Segments.BODY]: Joi.object().keys({
     avatar: Joi.string().uri().default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
   }),
-  headers: Joi.object().keys({
-    authorization: Joi.string().required(),
+  [Segments.HEADERS]: Joi.object().keys({
+    authorization: Joi.string(),
   }).unknown(true),
 }), auth, editAvatar);
 
